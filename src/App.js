3d9c5c50c85e5ff76a4cec44cc55335b77ae9e35
RockyBoun <rockyboun2001@gmail.com>
2022-07-07 18:18:49 +0900

    react-page-sample

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ const App = () => {
     <div>
       <Header />
       <Routes>
-        <Route path="/signup" element={SignupPage} />
-        <Route path="/login" element={LoginPage} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
       <Footer />
     </div>
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,46 +4,38 @@ import PersonIcon from "@mui/icons-material/Person";
 import MapsHomeWorkIcon from "@mui/icons-material/MapsHomeWork";
 import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 import EmailIcon from "@mui/icons-material/Email";
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
     <div className="header">
       <div className="pages">
-        <li className="logo">
-          <a href="/">Logo</a>
-        </li>
-        <ul>
-          <li>
-            <a href="/team">
-              <PersonIcon />
-              TEAM
-            </a>
-          </li>
-          <li>
-            <a href="/work">
-              <MapsHomeWorkIcon />
-              WORK
-            </a>
-          </li>
-          <li>
-            <a href="/pricing">
-              <AttachMoneyIcon />
-              PRICING
-            </a>
-          </li>
-          <li>
-            <a href="/contact">
-              <EmailIcon />
-              CONTACT
-            </a>
-          </li>
-          <li>
-            <a href="/about">ABOUT</a>
-          </li>
-        </ul>
+        <div className="logo">
+          <button>Logo</button>
+        </div>
+        <div className="page-title">
+          <button>
+            <PersonIcon />
+            TEAM
+          </button>
+          <button>
+            <MapsHomeWorkIcon />
+            WORK
+          </button>
+          <button>
+            <AttachMoneyIcon />
+            PRICING
+          </button>
+          <button>
+            <EmailIcon />
+            CONTACT
+          </button>
+          <button>ABOUT</button>
+        </div>
       </div>
       <div className="sign">
-        <a href="/login">Log In</a>
-        <a href="/signup">Sign Up</a>
+        <NavLink to="/login">Log In</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
       </div>
     </div>
   );
